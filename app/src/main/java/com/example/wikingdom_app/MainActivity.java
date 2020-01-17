0c3582b46dd27250949415281714f48972ae58c6
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-01-17 09:09:19 +0100

    Skapat en JSON parser.

--- app/src/main/java/com/example/wikingdom_app/MainActivity.java
+++ app/src/main/java/com/example/wikingdom_app/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.wikingdom_app;
 
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -20,10 +28,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +49,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
+
+        mQueue = Volley.newRequestQueue(this);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                jsonParse();
             }
+
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -51,6 +71,59 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void jsonParse(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.130.216.101/TP/api.php"; //http://webhook.site/40b51970-0aef-4c5e-84c4-358e41aa3b67
+
+        StringRequest strRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        try {
+                            JSONArray jsonArray = new JSONObject(response).getJSONArray("sidor");
+                            Log.d("JSON", "------------------------------------------\n\n");
+
+                            for(int i = 0; i < jsonArray.length(); i++){
+                                JSONObject page = jsonArray.getJSONObject(i);
+
+                                String id = page.getString("id");
+                                String title = page.getString("titel");
+                                String date = page.getString("datum");
+
+                                Log.d("JSON", "#" + id + " " + title + ", " + date + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        error.printStackTrace();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("nyckel", "x9qXnagSOsS6sHun");
+                params.put("tjanst", "wiki");
+                params.put("typ", "JSON");
+                params.put("wiki", "9");
+                return params;
+            }
+        };
+
+        queue.add(strRequest);
+
     }
 
     @Override
