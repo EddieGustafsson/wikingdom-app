03b373bd4d909ab4e44b01898b93ca831d3f2871
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2020-01-31 08:49:13 +0100

    Har lagt till funktionalitet på bold, italic, header, och quote.

--- app/src/main/java/com/example/wikingdom_app/EditActivity.java
+++ app/src/main/java/com/example/wikingdom_app/EditActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -71,27 +78,72 @@ public class EditActivity extends AppCompatActivity {
     }
 
     public void setMode(String mode){
+        TextInputEditText textInputEditText = findViewById(R.id.textEditor);
+        int startSelection = textInputEditText.getSelectionStart();
+        int endSelection = textInputEditText.getSelectionEnd();
+        String allText = Objects.requireNonNull(textInputEditText.getText()).toString();
+        String selectedText = allText.substring(startSelection, endSelection);
+        //final SpannableStringBuilder stringBuilder = new SpannableStringBuilder(textInputEditText.getText().toString());
         switch(mode){
             case "bold":
-                
+                if(!selectedText.isEmpty()) {
+                    String editedText = "**" + selectedText + "**";
+                    String editText = allText.replace(selectedText, editedText);
+                    textInputEditText.setText(editText);
+                }else{
+                    int currentPosition = startSelection;
+                    String emptyBold = "****";
+                    textInputEditText.getText().insert(currentPosition, emptyBold);
+                    textInputEditText.setSelection(currentPosition);
+                }
+                /*final StyleSpan boldText = new StyleSpan(android.graphics.Typeface.BOLD);
+                stringBuilder.setSpan(boldText, startSelection, endSelection, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                textInputEditText.setText(stringBuilder);*/
                 break;
             case "italic":
-
+                if(!selectedText.isEmpty()) {
+                    String editedText = "*" + selectedText + "*";
+                    String editText = allText.replace(selectedText, editedText);
+                    textInputEditText.setText(editText);
+                }else{
+                    int currentPosition = startSelection;
+                    String emptyBold = "**";
+                    textInputEditText.getText().insert(currentPosition, emptyBold);
+                    textInputEditText.setSelection(currentPosition);
+                }
                 break;
             case "header":
-
+                if(!selectedText.isEmpty()) {
+                    String editedText = "#" + selectedText + "#";
+                    String editText = allText.replace(selectedText, editedText);
+                    textInputEditText.setText(editText);
+                }else{
+                    int currentPosition = startSelection;
+                    String emptyBold = "##";
+                    textInputEditText.getText().insert(currentPosition, emptyBold);
+                    textInputEditText.setSelection(currentPosition);
+                }
                 break;
             case "quote":
-
+                if(!selectedText.isEmpty()) {
+                    String editedText = ">" + selectedText;
+                    String editText = allText.replace(selectedText, editedText);
+                    textInputEditText.setText(editText);
+                }else{
+                    int currentPosition = startSelection;
+                    String emptyBold = ">";
+                    textInputEditText.getText().insert(currentPosition, emptyBold);
+                    textInputEditText.setSelection(currentPosition);
+                }
                 break;
             case "linebreak":
-
+                Toast.makeText(EditActivity.this, "Linebreaks are not implemented yet...", Toast.LENGTH_SHORT).show();
                 break;
             case "link":
-
+                Toast.makeText(EditActivity.this, "Links are not implemented yet...", Toast.LENGTH_SHORT).show();
                 break;
             case "image":
-
+                Toast.makeText(EditActivity.this, "Images are not implemented yet...", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
